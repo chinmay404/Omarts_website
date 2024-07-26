50c52212493970dbed46c5a362301769dd71d385
chinmay404 <92822013+chinmay404@users.noreply.github.com>
2024-07-26 19:06:56 +0530

    a
    as

---
+++ script.js
@@ -0,0 +1,36 @@
+function svgDraw() {
+	let ganesh = document.querySelectorAll("#ganesh path");
+
+	ganesh.forEach(i => {
+		i.style.fill = "none";
+	});
+
+	ganesh.forEach((item, i) => {
+		let pathLength = ganesh.item(i).getTotalLength();
+		item.setAttribute("id", `p${i}`);
+		let itemId = document.getElementById(`p${i}`);
+		itemId.style.strokeDasharray = pathLength;
+		itemId.style.strokeDashoffset = pathLength;
+		itemId.style.animation = "line-anim 5s ease-in-out";
+	});
+
+	setTimeout(() => {
+		ganesh.forEach((item, i) => {
+			let pathLength = ganesh.item(i).getTotalLength();
+			item.removeAttribute("style");
+		});
+		// Start fade-out animation
+		document.body.classList.add('fade-out');
+		// Redirect to index.html after the fade-out animation completes
+		setTimeout(() => {
+			window.location.href = '/home.html';
+		}, 1000); // Match the duration of the fade-out animation
+	}, 5000);
+}
+
+// Play audio on page load
+window.onload = function () {
+	svgDraw();
+	var audio = document.getElementById("audio");
+	audio.play();
+};
